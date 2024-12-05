384ce6eadb247e4cf36b8a32ae6e2b89c02e52f9
vntrungtin1909 <ngotrungtin1992003@gmail.com>
2024-12-05 17:52:44 +0700

    quên mật khẩu

--- TicketSupport/ViewModels/ChangePassVM.cs
+++ TicketSupport/ViewModels/ChangePassVM.cs
@@ -8,9 +8,7 @@ namespace TicketSupport.ViewModels
 {
     public class ChangePassVM
     {
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress]
-        public string Email { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match")]
