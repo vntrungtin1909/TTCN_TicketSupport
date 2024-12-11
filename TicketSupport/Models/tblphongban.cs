b87437a07ad2007ed9e66e930f3a486c798634ed
HoaiMinh <quangluatlm@gmail.com>
2024-12-11 19:55:32 +0700

    Cap nhat front dang nhap

--- TicketSupport/Models/tblphongban.cs
+++ TicketSupport/Models/tblphongban.cs
@@ -33,6 +33,6 @@ namespace TicketSupport.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tblnguoidung> tblnguoidungs { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<tblquyen> tblquyens { get; set; }		
-	}
+        public virtual ICollection<tblquyen> tblquyens { get; set; }
+    }
 }
